f1f5b1407d06dfcec45a355c47e327756ba4d313
Hasoň Filip <hasonfilip@seznam.cz>
2023-07-19 09:58:34 +0000

    Update file manager.py

---
+++ manager.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+""" 
+TODO název
+Autor: Filip Hasoň
+zápočtový program
+letní semestr 2022/23
+Předmět: NPRG031 Programování 2
+"""
+def main(): 
+    import tkinter as tk
+    root=tk.Tk()#screenName="okno")
+    root.state('zoomed') 
+    root.title('Manager')
+
+
+    # Configure column weight for relative width
+    root.grid_columnconfigure(0, weight=1)
+
+    # frames,colrs = [0,0,0],["red","green","blue"]
+    
+    # for i in range(3):
+    #     frames[i]=tk.Frame(root, width=200, height=100, bg=colrs[i])
+    #     frames[i].grid(row=i, column=0, pady=(10,0), padx=10, sticky="ew")
+
+
+    def button1_click():
+        print("Button 1 clicked!")
+
+    def button2_click():
+        print("Button 2 clicked!")
+
+
+
+    # Create the first frame
+    frame1 = tk.Frame(root, width=200, height=100, borderwidth=2, relief=tk.RAISED)
+    frame1.grid(row=1, column=0, pady=10, padx=10, sticky="ew")
+
+    # Create the first button
+    button1 = tk.Button(frame1, text="Button 1", command=button1_click)
+    button1.pack(side="right", padx=5, pady=5)
+
+    # Create the second button
+    button2 = tk.Button(frame1, text="Button 2", command=button2_click)
+    button2.pack(side="right", padx=5, pady=5)
+
+    # Create the second frame
+    frame2 = tk.Frame(root, width=200, height=100, bg="blue")
+    frame2.grid(row=2, column=0, pady=10, padx=10, sticky="ew")
+
+    # Create the third frame
+    frame3 = tk.Frame(root, width=200, height=100, bg="green")
+    frame3.grid(row=3, column=0, pady=10, padx=10, sticky="ew")
+
+
+   # print(type(frame1))
+
+
+
+    # first = tk.Canvas(root,background="green",height=root.winfo_height()+10,width=root.winfo_width()+10 )
+    # first.pack()
+    # button = tk.Button(root, text='Stop', width=25, command=root.destroy)
+
+    # button.pack()
+
+    def button_clicked():
+        print("Button clicked!")
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
